18894e8edf13038189f33c881161c4bfdcb4ec9f
Ian <iansm643@gmail.com>
2025-10-03 10:34:21 -0400

    Solved fibonacci sequence problem.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Prints the first n Fibonacci numbers
 const printFibonacci = (n) => {
- 
+    let num1 = 0;
+    let num2 = 1;
+    let next = 0;
+    for (let i = 0; i < n; i++) {
+        console.log(next);
+        num1 = num2;
+        num2 = next;
+        next = num1 + num2;
+    }
+
 }
 
 printFibonacci(5);  // should print 0, 1, 1, 2, 3
